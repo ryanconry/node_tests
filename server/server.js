343eb1b3f80d307e1c21bee77075a9610db8285f
Ryan Conry <r.conry@hotmail.com>
2018-05-18 11:30:18 -0400

    implemented supertest to test express server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
 const express = require('express');
 
+
 var app = express();
 
 app.get('/', (req,res) => {
-  res.send('Hello World!');
+  res.status(404).send({
+    error: 'page not found',
+    name: 'app'
+  });
+});
+
+app.get('/users', (req,res) => {
+  res.status(200).send([{
+    name: 'Ryan',
+    age: 27
+  },{
+    name: 'user1',
+    age: 30
+  },{
+    name: 'user2',
+    age: 35
+  }]);
 });
 
 app.listen(3000);
+
+module.exports.app = app;
---
+++ server/server.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');  //assertion testing express
+const expect = require('expect');
+
+var app = require('./server.js').app;
+
+it('should return Hello World response', (done) => {   //done identifiies async test
+  request(app)
+    .get('/')     //.get for get request, param is url
+    .expect((res) => {              //expect method of supertest
+      expect(res.body).toInclude({error: 'page not found'})   //expect library
+    })
+    .expect(404)      //assertion test for status code
+    .end(done);     //end test withd one
+});
+
+it('should return array of users',(done) => {
+  request(app)
+    .get('/users')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body).toInclude({name: 'Ryan', age: 27})
+    })
+    .end(done)
+});
