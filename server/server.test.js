7f34c5c149efbe6d49bf6e0ad9fc2a9f8fda6b19
Ryan Conry <r.conry@hotmail.com>
2018-05-18 15:08:03 -0400

    describe() to identify test suites

--- server/server.test.js
+++ server/server.test.js
@@ -3,22 +3,24 @@ const expect = require('expect');
 
 var app = require('./server.js').app;
 
-it('should return Hello World response', (done) => {   //done identifiies async test
-  request(app)
-    .get('/')     //.get for get request, param is url
-    .expect((res) => {              //expect method of supertest
-      expect(res.body).toInclude({error: 'page not found'})   //expect library
-    })
-    .expect(404)      //assertion test for status code
-    .end(done);     //end test withd one
-});
+describe('Server', () => {
+  it('should return Hello World response', (done) => {   //done identifiies async test
+    request(app)
+      .get('/')     //.get for get request, param is url
+      .expect((res) => {              //expect method of supertest
+        expect(res.body).toInclude({error: 'page not found'})   //expect library
+      })
+      .expect(404)      //assertion test for status code
+      .end(done);     //end test withd one
+  });
 
-it('should return array of users',(done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({name: 'Ryan', age: 27})
-    })
-    .end(done)
+  it('should return array of users',(done) => {
+    request(app)
+      .get('/users')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toInclude({name: 'Ryan', age: 27})
+      })
+      .end(done)
+  });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,47 +1,56 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers',() => {       //it() being a mocha test case
-  var res = utils.add(33,11);
-  expect(res).toBe(44).toBeA('number');   //assertion test using expect library
-});
 
-it('should square the given number', () => {
-  var res = utils.square(9);
-  expect(res).toBe(81).toBeA('number');
-});
+describe('Utils', () => {                     //identify test suites
+  describe('#math',() => {
+    it('should add two numbers',() => {       //it() being a mocha test case
+      var res = utils.add(33,11);
+      expect(res).toBe(44).toBeA('number');   //assertion test using expect library
+    });
 
-it('should expect some values', () => {         //assertion testing for objects/arrays
-  expect({name: 'Ryan'}).toEqual({name: 'Ryan'}); //toEqual instead of toBe
-  expect([2,3,4]).toInclude(3);
-  expect({
-    name: 'Ryan',
-    age: 27
-  }).toInclude({name: 'Ryan'});
-  expect({
-    name: 'Ryan',
-    age: 27
-  }).toExclude({likes: 'football'});
-});
-
-it('should verify first and last names are set', () => {
-  var user = utils.setName({age: 27, location: 'Hamilton'}, 'Ryan Conry')
-  expect(user).toInclude({
-    firstName : 'Ryan',
-    lastName : 'Conry'
-  });
-});
-
-it('Should async add two numbers', (done) => {    //done specifies async test
-  utils.asyncAdd(4,3,(sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
-  });
-});
-
-it('Should async square a number', (done) => {
-  utils.asyncSquare(7,(squared) => {
-    expect(squared).toBe(49).toBeA('number');
-    done();
+    it('should square the given number', () => {
+      var res = utils.square(9);
+      expect(res).toBe(81).toBeA('number');
+    });
   })
-})
+
+  describe('#objects', () => {
+    it('should expect some values', () => {         //assertion testing for objects/arrays
+      expect({name: 'Ryan'}).toEqual({name: 'Ryan'}); //toEqual instead of toBe
+      expect([2,3,4]).toInclude(3);
+      expect({
+        name: 'Ryan',
+        age: 27
+      }).toInclude({name: 'Ryan'});
+      expect({
+        name: 'Ryan',
+        age: 27
+      }).toExclude({likes: 'football'});
+    });
+
+    it('should verify first and last names are set', () => {
+      var user = utils.setName({age: 27, location: 'Hamilton'}, 'Ryan Conry')
+      expect(user).toInclude({
+        firstName : 'Ryan',
+        lastName : 'Conry'
+      });
+    });
+  });
+
+  describe('#async', () => {
+    it('Should async add two numbers', (done) => {    //done specifies async test
+      utils.asyncAdd(4,3,(sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+      });
+    });
+
+    it('Should async square a number', (done) => {
+      utils.asyncSquare(7,(squared) => {
+        expect(squared).toBe(49).toBeA('number');
+        done();
+      });
+    });
+  });
+});
